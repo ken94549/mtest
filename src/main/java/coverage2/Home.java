cba7b299731c30b6afcbefcde486649bbe335278
jyu <jun.yu@am.sony.com>
2016-08-08 14:57:36 -0700

    Add home

---
+++ src/main/java/coverage2/Home.java
@@ -0,0 +1,15 @@
+package coverage2;
+
+public class Home {
+    private String name;
+    private String address1;
+    
+    public Home(String name,String address1){
+        this.name=name;
+        this.address1=address1;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
