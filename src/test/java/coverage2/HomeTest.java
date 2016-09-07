20740276d4da8a2b7e2cf87408bccca9dc1f9bb6
zdeng <zdeng@us9801a7a035b7.am.sony.com>
2016-09-07 16:22:52 -0700

    add a class

---
+++ src/test/java/coverage2/HomeTest.java
@@ -0,0 +1,18 @@
+package coverage2;
+
+import org.testng.annotations.Test;
+
+import coverage2.Home;
+    
+public class HomeTest {
+    
+    @Test(groups={"function"})
+    public void testHome() {
+        Home home = new Home("my home", "619");
+        String address=home.getAddress();
+        String name=home.getName();
+        home.setName(name);
+        home.setAddress(address);
+    }
+
+}
